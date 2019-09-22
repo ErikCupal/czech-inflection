bb2d4990395d35d993a4480f0e9410673f39e3bb
Erik Cupal <erik@cup.al>
2019-09-22 17:02:54 +0200

    Implement library

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -2,7 +2,12 @@ import inflect from '..'
 
 const CASES = [1, 2, 3, 4, 5, 6, 7]
 
-const testInflections = ({ word, plural, animate, expectedInflections }) => {
+const testInflections = ({
+  word,
+  plural = false,
+  animate = false,
+  expectedInflections,
+}) => {
   expect(
     CASES.map(grammarCase => inflect({ word, grammarCase, plural, animate })),
   ).toEqual(expectedInflections)
@@ -11,19 +16,18 @@ const testInflections = ({ word, plural, animate, expectedInflections }) => {
 describe('inflect', () => {
   it('inflects Tomáš correctly', () => {
     const WORD = 'Tomáš'
-
     testInflections({
       word: WORD,
       plural: false,
       animate: true,
       expectedInflections: [
         'Tomáš',
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
+        'Tomáše',
+        'Tomášovi',
+        'Tomáše',
+        'Tomáši',
+        'Tomášovi',
+        'Tomášem',
       ],
     })
     testInflections({
@@ -31,13 +35,339 @@ describe('inflect', () => {
       plural: true,
       animate: true,
       expectedInflections: [
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
-        'Tomáš',
+        'Tomášové',
+        'Tomášů',
+        'Tomášům',
+        'Tomáše',
+        'Tomášové',
+        'Tomáších',
+        'Tomáši',
+      ],
+    })
+  })
+
+  it('inflects hrad correctly', () => {
+    const WORD = 'hrad'
+    testInflections({
+      word: WORD,
+      plural: false,
+      expectedInflections: [
+        'hrad',
+        'hradu',
+        'hradu',
+        'hrad',
+        'hrade',
+        'hradu',
+        'hradem',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      expectedInflections: [
+        'hrady',
+        'hradů',
+        'hradům',
+        'hrady',
+        'hrady',
+        'hradech',
+        'hrady',
+      ],
+    })
+  })
+
+  it('inflects Angelika correctly', () => {
+    const WORD = 'Angelika'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Angelika',
+        'Angeliky',
+        'Angelice',
+        'Angeliku',
+        'Angeliko',
+        'Angelice',
+        'Angelikou',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: [
+        'Angeliky',
+        'Angelik',
+        'Angelikám',
+        'Angeliky',
+        'Angeliky',
+        'Angelikách',
+        'Angelikami',
+      ],
+    })
+  })
+
+  it('inflects Jan correctly', () => {
+    const WORD = 'Jan'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Jan',
+        'Jana',
+        'Janovi',
+        'Jana',
+        'Jane',
+        'Janovi',
+        'Janem',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: [
+        'Janové',
+        'Janů',
+        'Janům',
+        'Jany',
+        'Janové',
+        'Janech',
+        'Jany',
+      ],
+    })
+  })
+
+  it('inflects Josef correctly', () => {
+    const WORD = 'Josef'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Josef',
+        'Josefa',
+        'Josefovi',
+        'Josefa',
+        'Josefe',
+        'Josefovi',
+        'Josefem',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: [
+        'Josefové',
+        'Josefů',
+        'Josefům',
+        'Josefy',
+        'Josefové',
+        'Josefech',
+        'Josefy',
+      ],
+    })
+  })
+
+  it('inflects Zeus correctly', () => {
+    const WORD = 'Zeus'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Zeus',
+        'Dia',
+        'Diovi',
+        'Dia',
+        'Die',
+        'Diovi',
+        'Diem',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: ['Diové', 'Diů', 'Diům', '', 'Diové', '', ''],
+    })
+  })
+
+  it('inflects Monika correctly', () => {
+    const WORD = 'Monika'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Monika',
+        'Moniky',
+        'Monice',
+        'Moniku',
+        'Moniko',
+        'Monice',
+        'Monikou',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: [
+        'Moniky',
+        'Monik',
+        'Monikám',
+        'Moniky',
+        'Moniky',
+        'Monikách',
+        'Monikami',
+      ],
+    })
+  })
+
+  it('inflects Čtyři correctly', () => {
+    const WORD = 'Čtyři'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Čtyři',
+        'Čtyřech',
+        'Čtyřem',
+        'Čtyři',
+        'Čtyři',
+        'Čtyřech',
+        'Čtyřmi',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: ['', '', '', '', '', '', ''],
+    })
+  })
+
+  it('inflects Monika correctly', () => {
+    const WORD = 'Marcel'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Marcel',
+        'Marcela',
+        'Marcelovi',
+        'Marcela',
+        'Marceli',
+        'Marcelovi',
+        'Marcelem',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: [
+        'Marcelové',
+        'Marcelů',
+        'Marcelům',
+        'Marcely',
+        'Marcelové',
+        'Marcelích',
+        'Marcely',
+      ],
+    })
+  })
+
+  it('inflects Dagmar correctly', () => {
+    const WORD = 'Dagmar'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Dagmar',
+        'Dagmary',
+        'Dagmaře',
+        'Dagmar',
+        'Dagmar',
+        'Dagmar',
+        'Dagmar',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: [
+        'Dagmary',
+        'Dagmar',
+        'Dagmarám',
+        'Dagmary',
+        'Dagmary',
+        'Dagmarách',
+        'Dagmarami',
+      ],
+    })
+  })
+
+  it('inflects Ivo correctly', () => {
+    const WORD = 'Ivo'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: [
+        'Ivo',
+        'Iva',
+        'Ivovi',
+        'Iva',
+        'Ivo',
+        'Ivovi',
+        'Ivem',
+      ],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: [
+        'Ivové',
+        'Ivů',
+        'Ivům',
+        'Ivy',
+        'Ivové',
+        'Ivech',
+        'Ivy',
+      ],
+    })
+  })
+
+  it('inflects Iva correctly', () => {
+    const WORD = 'Iva'
+    testInflections({
+      word: WORD,
+      plural: false,
+      animate: true,
+      expectedInflections: ['Iva', 'Ivy', 'Ivě', 'Ivu', 'Ivo', 'Ivě', 'Ivou'],
+    })
+    testInflections({
+      word: WORD,
+      plural: true,
+      animate: true,
+      expectedInflections: [
+        'Ivy',
+        'Iv',
+        'Ivám',
+        'Ivy',
+        'Ivy',
+        'Ivách',
+        'Ivami',
       ],
     })
   })
--- src/index.js
+++ src/index.js
@@ -1,10 +1,153 @@
+import { patterns, exceptions, forceM, forceF, forceS } from './constants'
+
+// /**
+//  * @param string pattern
+//  * @param string word
+//  *
+//  * @return int position from left where the pattern matched from right otherwise -1
+//  */
+const match = (pattern, word, replacements) => {
+  // if (pattern ==='-([cčďšňřťž])') {
+    // console.log({pattern, word, sub: pattern.substring(1), reg: new RegExp(pattern.substring(1) + '$', 'iu')})
+  // }
+  if (pattern.substring(0, 1) !== '-') {
+    // compare if the first byte is ascii `-`
+    return pattern.toLocaleLowerCase() === word.toLocaleLowerCase() ? 0 : -1
+  }
+
+  const matches = new RegExp(pattern.substring(1) + '$', 'iu').exec(word)
+  if (matches) {
+    for (let i = matches.length - 1; i > 0; i--) {
+      replacements[i - 1] = matches[matches.length - i]
+    }
+
+    return word.length - matches[0].length
+  }
+
+  return -1
+}
+
+const breakAccents = word => {
+  let newWord = word
+
+  const replacementsArr = [
+    ['di', 'ďi'],
+    ['ti', 'ťi'],
+    ['ni', 'ňi'],
+    ['dě', 'ďe'],
+    ['tě', 'ťe'],
+    ['ně', 'ňe'],
+  ]
+  replacementsArr.forEach(([pattern, replacement]) => {
+    newWord = newWord.split(pattern).join(replacement)
+  })
+
+  return newWord
+}
+
+const fixAccents = word => {
+  let newWord = word
+
+  const replacementsArr = [
+    ['ďi', 'di'],
+    ['ťi', 'ti'],
+    ['ňi', 'ni'],
+    ['ďe', 'dě'],
+    ['ťe', 'tě'],
+    ['ňe', 'ně'],
+  ]
+  replacementsArr.forEach(([pattern, replacement]) => {
+    newWord = newWord.split(pattern).join(replacement)
+  })
+
+  return newWord
+}
+
 export const inflect = ({
   word,
   grammarCase,
   plural = false,
-  animate = false
+  animate = false,
 }) => {
-  return ''
+  const originalWord = word
+  if (plural) {
+    grammarCase += 7
+  }
+
+  if (grammarCase === 1) {
+    return word
+  }
+
+  let gender
+
+  const firstLetter = word.substring(0, 1)
+  const isUpperCase = firstLetter.toLocaleUpperCase() === firstLetter
+
+  word = breakAccents(word)
+  const lowercaseWord = word.toLocaleLowerCase()
+
+  if (forceM.includes(lowercaseWord)) {
+    gender = 'm'
+  } else if (forceF.includes(lowercaseWord)) {
+    gender = 'f'
+  } else if (forceS.includes(lowercaseWord)) {
+    gender = 's'
+  }
+
+  let exception
+  for (const e of exceptions) {
+    if (lowercaseWord === e[0]) {
+      exception = e
+      break
+    }
+  }
+
+
+  let replacements = []
+  for (const pattern of patterns) {
+    if (gender && pattern[0] !== gender) {
+      continue
+    }
+
+    word = exception ? exception[1] : word
+    const left = match(pattern[1], word, replacements)
+
+
+    if (left !== -1) {
+      // console.log({ left, pattern, word, exception, lowercaseWord, gender, animate, plural, grammarCase, replacements })
+      const prefix = word.substring(0, left)
+
+      if (exception && grammarCase === 4) {
+        return exception[2]
+      }
+
+      let postfix = pattern[grammarCase]
+      if (postfix === undefined) {
+        return ''
+      }
+      // console.log({postfix, replacements, pattern})
+      replacements.forEach((replacement, i) => {
+        postfix = postfix.replace(new RegExp(String(i), 'g'), replacement)
+      })
+
+      const posSlash = postfix.indexOf('/')
+      if (posSlash !== -1) {
+        if (animate) {
+          postfix = postfix.substring(posSlash + 1)
+        } else {
+          postfix = postfix.substring(0, posSlash)
+        }
+      }
+
+      let result = fixAccents(prefix + postfix)
+      if (isUpperCase) {
+        result = result.substring(0, 1).toLocaleUpperCase() + result.substring(1)
+      }
+      return result
+    }
+  }
+
+  return originalWord
 }
 
 export default inflect
