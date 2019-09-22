3808b5e117622a3aebcbcd7dc6a0c88eba115cfc
Erik Cupal <erik@cup.al>
2019-09-22 20:58:07 +0200

    Updates

---
+++ precache-manifest.b92746ac37f17fa4ee6708908ab34296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31daf1eef3d1739b4c41a4c8289b226b",
+    "url": "/czech-inflection/index.html"
+  },
+  {
+    "revision": "c6cc9dffb913b87fb165",
+    "url": "/czech-inflection/static/css/2.23bc0b14.chunk.css"
+  },
+  {
+    "revision": "44507553db04f6c84df1",
+    "url": "/czech-inflection/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c6cc9dffb913b87fb165",
+    "url": "/czech-inflection/static/js/2.8b8ae70e.chunk.js"
+  },
+  {
+    "revision": "44507553db04f6c84df1",
+    "url": "/czech-inflection/static/js/main.76cbe573.chunk.js"
+  },
+  {
+    "revision": "da307b07099d73d1fa5f",
+    "url": "/czech-inflection/static/js/runtime-main.3226c448.js"
+  }
+]);
